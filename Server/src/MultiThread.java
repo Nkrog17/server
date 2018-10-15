aa752ab1641748ee427939b97c12dd85045e3309
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2018-10-15 14:33:55 +0200

    hej

--- Server/src/MultiThread.java
+++ Server/src/MultiThread.java
@@ -18,7 +18,17 @@ public class MultiThread implements Runnable {
 			
 			
 			while(true) {
+				double amount = fromClient.readDouble();
+				int years = fromClient.readInt();
+				double annualInterestRate = fromClient.readDouble();
 				
+				double monthlyInterestRate = annualInterestRate / 1200;
+				double monthlyPayment = amount * monthlyInterestRate / (1 - 
+						(1 / Math.pow(1 + monthlyInterestRate, years * 12)));
+				toClient.writeDouble(monthlyPayment);
+				
+				double totalPayment = monthlyPayment * years * 12;
+				toClient.writeDouble(totalPayment);
 			}
 		} catch(IOException e) {}
 	}
