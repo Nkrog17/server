023648d8469667b723aa65258b71de9f969160f0
Niklasskjold <35644289+Niklasskjold@users.noreply.github.com>
2018-10-16 10:49:57 +0200

    ClientName

--- Server/src/PlayerThread.java
+++ Server/src/PlayerThread.java
@@ -3,10 +3,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.*;
 
-public class MultiThread implements Runnable {
+public class PlayerThread implements Runnable {
 	private Socket socket;
 
-	public MultiThread(Socket socket) {
+	public PlayerThread(Socket socket) {
 		this.socket = socket;
 	}
 
@@ -17,19 +17,10 @@ public class MultiThread implements Runnable {
 			DataOutputStream toClient = new DataOutputStream(socket.getOutputStream());
 			
 			
-			while(true) {
-				double amount = fromClient.readDouble();
-				int years = fromClient.readInt();
-				double annualInterestRate = fromClient.readDouble();
-				
-				double monthlyInterestRate = annualInterestRate / 1200;
-				double monthlyPayment = amount * monthlyInterestRate / (1 - 
-						(1 / Math.pow(1 + monthlyInterestRate, years * 12)));
-				toClient.writeDouble(monthlyPayment);
-				
-				double totalPayment = monthlyPayment * years * 12;
-				toClient.writeDouble(totalPayment);
-			}
+			
+				String playerName=fromClient.readUTF();
+				System.out.println(playerName);
+			
 		} catch(IOException e) {}
 	}
 		
