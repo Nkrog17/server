fcd4e6755bfc88f8a2391fc194180bbd87f89b76
Niels <nkrog17@student.aau.dk>
2018-10-23 13:21:33 +0200

    Reset to previous commit

---
+++ Testingtest/src/Test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String[] messages = new String[50];
+		
+		messages[0] = "0";
+		
+
+	}
+
+}
---
+++ testing/src/testing/Testing.java
@@ -0,0 +1,11 @@
+package testing;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		String test = new String("testing\nEhehe");
+		System.out.println(test);
+
+	}
+
+}
